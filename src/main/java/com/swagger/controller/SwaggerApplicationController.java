f7a13dc460d7ed0f56f843a8c9256198b0eeb760
[AlmasBanuA2011] <[almasbanu3304@gmail.com]>
2022-05-05 00:40:20 +0530

    Added: Swagger UI in Practice Problem

---
+++ src/main/java/com/swagger/controller/SwaggerApplicationController.java
@@ -0,0 +1,54 @@
+package com.swagger.controller;
+
+import com.swagger.model.Person;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/swagger")
+public class SwaggerApplicationController {
+
+    Map<Integer, Person> personMap = new HashMap<>();
+
+    @PostMapping(path = "/create")
+    public Map createPersonData(@RequestBody Person person) {
+        personMap.put(person.getId(), person);
+        return personMap;
+    }
+
+    @GetMapping("/getAll")
+    public Map getAllPersonsData() {
+        return personMap;
+    }
+
+    @PutMapping("/updatePerson")
+    public Map updatePersonData(@RequestBody Person person) {
+        List<Person> personList = new ArrayList<>(personMap.values());
+        for (int i = 0; i < personList.size(); i++) {
+            if (personList.get(i).getId() == person.getId()) {
+                personMap.put(person.getId(), person);
+            }
+        }
+        return personMap;
+    }
+
+    @DeleteMapping("/deletePerson/{id}")
+    public String deletePersonData(@PathVariable Integer id) {
+        List<Person> personList = new ArrayList<>(personMap.values());
+        for (int i = 0; i < personList.size(); i++) {
+            if (personList.get(i).getId() == id) {
+                personMap.remove(id);
+                return "delete Successfully";
+            }
+
+        }
+        return "id not found";
+
+    }
+}
+
+
